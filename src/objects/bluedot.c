6456ccdb9cfee83caa224485f1b562de5af98614
Kamil Kalata <kamil.kalata@protonmail.com>
2025-01-17 17:30:15 +0100

    Move common dot functions to separate file

--- src/objects/bluedot.c
+++ src/objects/bluedot.c
@@ -1,36 +1,5 @@
 #include "../snake.h"
 
-void PlaceDot(
-    Position *dotPos,
-    Snake *snake,
-    Position otherDotPos
-)
-{
-    int dotUnderSomeObject;
-    do
-    {   
-        dotUnderSomeObject = 0;
-        dotPos->x = rand() % BOARD_SECTION_WIDTH; // random number between 0 and board width
-        dotPos->y = rand() % BOARD_SECTION_HEIGHT; // random number between 0 and board height
-        
-        SnakeSegment *snakeSegment = snake->segment;
-        do
-        {
-            if (snakeSegment->pos.x == dotPos->x && snakeSegment->pos.y == dotPos->y)
-            {
-                dotUnderSomeObject = 1;
-                break;
-            }
-            snakeSegment = snakeSegment->next;
-        } while (snakeSegment != snake->segment);
-
-        if (otherDotPos.x == dotPos->x && otherDotPos.y == dotPos->y)
-        {
-            dotUnderSomeObject = 1;
-        }
-    } while (dotUnderSomeObject);
-}
-
 void PlaceBlueDot(
     BlueDot *blueDot,
     Snake *snake,
@@ -38,19 +7,6 @@ void PlaceBlueDot(
 )
 {
     PlaceDot(&blueDot->pos, snake, redDot->pos);
-}
-
-void RenderDot(
-    GameWindow *window,
-    const Position pos
-)
-{
-    SDL_Rect dotRect;
-    dotRect.x = window->rect.board.x + pos.x * SNAKE_SEGMENT_SIZE;
-    dotRect.y = window->rect.board.y + pos.y * SNAKE_SEGMENT_SIZE;
-    dotRect.w = SNAKE_SEGMENT_SIZE;
-    dotRect.h = SNAKE_SEGMENT_SIZE;
-    SDL_RenderFillRect(window->renderer, &dotRect);
 }
 
 void RenderBlueDot(
